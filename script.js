51c03248ad2c69116803c1a582c2a848c38bfe3c
Guitar <kittipong.sataya@gmail.com>
2024-05-13 14:33:15 +0700

    submit

---
+++ script.js
@@ -0,0 +1,18 @@
+const pencil = {
+    product_id: '1',
+    product_name: 'Apple Pencel',
+    quanlity_in_stock: 3000,
+    unit_price: '50 $',
+    product_description: {
+        product_brand: 'apple',
+        size: 'small',
+        colour: 'white',
+        additional_description: 'Apple pencil for ipad gen1'
+     }
+}
+
+console.log(pencil.product_id);     
+console.log(pencil.product_name);       
+console.log(pencil.quanlity_in_stock);    
+console.log(pencil.unit_price);
+console.log(pencil.product_description);
